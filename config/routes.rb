07449585e98dda4ccc3d08f881c4a4ad5a3cb05c
Andrew Kerr <me+github@andrewjkerr.com>
2016-04-25 12:15:44 -0400

    unbreak screensaver

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'screensaver#index'
 
+  get 'screensaver/index'
   get 'screensaver/new_image'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
